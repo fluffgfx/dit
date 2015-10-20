ca18f74374e1f50a63f91a56177aa9ddb6b6b2df
Kyle Fahringer <vulpino@users.noreply.github.com>
2015-10-19 22:30:32 -0400

    Trim the fat in preparation for a 0.1 release
    I'm keeping os_dotfiles so I can implement it in a 0.2 release without 0.1 dit repos breaking

--- lib/dit.rb
+++ lib/dit.rb
@@ -5,22 +5,18 @@ require 'json'
 
 class Dit < Thor
   desc "init", "Initialize the current directory as a dit directory."
-  def init()
-    # Some checks to see if this is already a .e or git repo
-    # You'll be stopped if this is already a .e repo
+  def init
     if(Dir.exist?(working_dir + "/.dit"))
       p "This is already a dit repo."
       return
-    end
-
-    if(Dir.exist?(working_dir + "/.git"))
+    elsif(Dir.exist?(working_dir + "/.git"))
       repo = Git.open(working_dir)
     else
       repo = Git.init(working_dir)
       puts "Initialized empty Git repository in #{File.join(working_dir, ".git")}"
     end
 
-    # Make a .e dir and a settings hash to be exported to a file in .e dir
+    # Make a .dit dir and a settings hash to be exported to a file in dit dir
     Dir.mkdir(working_dir + "/.dit")
     settings = {}
     
@@ -31,19 +27,16 @@ class Dit < Thor
       f.write ".dit/local_settings.json"
     end
 
-    # Write our changes to a JSON file in the .e dir
+    # Write our changes to a JSON file in the dit dir
     File.open(working_dir + "/.dit" + "/settings.json", "a") do |f|
       f.write settings.to_json if settings
     end
 
-    # commit changes as initial commit
-    # (so we can branch)
     repo.add(".dit/settings.json")
     repo.add(".gitignore")
     repo.commit("Dit inital commit")
 
-    clone_os_dotfiles 
-
+    clone_os_dotfiles
     puts "Initialized empty Dit repository in #{File.join(working_dir, ".dit")})"
   end
 
@@ -57,72 +50,35 @@ class Dit < Thor
     end
   end
 
-  desc "add FILE", "Add a dotfile to the working tree."
-  option :all, :type => :boolean, :aliases => '-A'
-  def add(f=nil)
-    options[:all] ? repo.add(:all=>true) : (repo.add(f) if f)
-    p "No file specified!" unless (options[:all] || f)
-  end
-
   option :message, :required => true, :aliases => '-m'
-  def commit()
+  def commit
     repo.commit(options[:message])
     symlink_unlinked
   end
 
-  desc "push", "Push your changes to a dit repository."
-  def push()
-    repo.branches.local.each do |b|
-      repo.branch(b).push
-    end
-  end
-
   desc "pull", "Pull your changes from a dit repo."
-  def pull()
+  def pull
     repo.pull
+    symlink_unlinked # In case there were changed files
+  end
+  
+  desc "rehash", "In case you ran a git command when you should've run a dit command."
+  def rehash
+    symlink_unlinked
   end
 
   private
 
-  def clone_os_dotfiles()
-    os = nil
-    if OS.windows?
-      os = 'windows'
-    elsif OS.x?
-      os = 'osx'
-    elsif OS.linux?
-      # cat to the rescue
-      distro = `cat /etc/*-release`
-      if distro.include?("Arch Linux")
-        os = "arch"
-      elsif distro.include?("debian")
-        os = "debian"
-      elsif distro.include?("gentoo")
-        os = "gentoo"
-      elsif distro.include?("redhat")
-        os = "redhat"
-      elsif distro.include?("SuSE")
-        os = "suse"
-      end
-    end
-
-    if Dir.exist?(".dit") 
-      file_url = "file:///" + File.absolute_path(Dir.getwd)
-      if os
-        Dir.chdir(".dit") do
-          os_dotfiles = Git.clone(file_url, "os_dotfiles")
-          os_dotfiles.branch(os).checkout
-        end
-      else
-        Dir.chdir(".dit") do
-          os_dotfiles = Git.clone(file_url, "os_dotfiles")
-          os_dotfiles.branch("master").checkout
-        end
-      end
+  def clone_os_dotfiles
+    # OS Specific Dotfiles Eventually TM
+    file_url = "file:///" + File.absolute_path(Dir.getwd)
+    Dir.chdir(".dit") do
+      os_dotfiles = Git.clone(file_url, "os_dotfiles")
+      os_dotfiles.branch("master").checkout
     end
   end
 
-  def symlink_all(files)
+  def symlink_all
     Dir.chdir(File.join(repo_name, ".dit", "os_dotfiles")) do
       Find.find('.') do |d|
         if File.directory?(d)
@@ -138,7 +94,7 @@ class Dit < Thor
     end
   end
 
-  def symlink_unlinked(settings)
+  def symlink_unlinked
     settings = nil
     begin
       settings = JSON.parse File.open(
@@ -179,5 +135,4 @@ class Dit < Thor
   def repo
     Git.open(working_dir)
   end
-
 end
